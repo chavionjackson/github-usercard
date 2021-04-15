971ab774d420a8a4c2e1d655b4f9d51eeb83148a
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-04-14 18:52:27 -0700

    Ran into API limit error

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,16 @@
+const card = document.querySelector('.cards');
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+// eslint-disable-next-line no-undef
+axios.get('https://api.github.com/users/chavionjackson')
+  .then(response => {
+    const info = response.data;
+    const create = theCards(info);
+    card.appendChild(create);
+  })
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,7 +36,22 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  'https://api.github.com/users/tetondan',
+  'https://api.github.com/users/dustinmyers',
+  'https://api.github.com/users/justsml',
+  'https://api.github.com/users/luishrd',
+  'https://api.github.com/users/bigknell',
+];
+
+followersArray.forEach(item => {
+  // eslint-disable-next-line no-undef
+  axios.get(`${item}`)  
+  .then(response => {
+  const newCard = theCards(response.data);
+  card.appendChild(newCard);
+  });
+})
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +72,49 @@ const followersArray = [];
       </div>
     </div>
 */
+
+const theCards = (object) => {
+  const div = document.createElement('div');
+  const img = document.createElement('img');
+  const div2 = document.createElement('div');
+  const h3 = document.createElement('h3');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const p3 = document.createElement('p');
+  const a = document.createElement('a');
+  const p4 = document.createElement('p');
+  const p5 = document.createElement('p');
+  const p6 = document.createElement('p');
+
+  div.classList.add('card');
+  div2.classList.add('card-info');
+  h3.classList.add('name');
+  p1.classList.add('username');
+
+  img.src = object.avatar_url;
+  h3.textContent = object.name;
+  p1.textContent = object.login;
+  p2.textContent = `Location: ${object.location}`;
+  p3.textContent = `Profile: ${object.url}`;
+  a.href = `${object.url}`; 
+  p4.textContent = `Followers: ${object.followers}`;
+  p5.textContent = `Following: ${object.following}`;
+  p6.textContent = `Bio: ${object.bio}`;
+
+  div.appendChild(img);
+  div.appendChild(div2);   
+  div.appendChild(h3);   
+  div.appendChild(p1);   
+  div.appendChild(p2);   
+  div.appendChild(p3);   
+  div.appendChild(a);   
+  div.appendChild(p4);   
+  div.appendChild(p5);   
+  div.appendChild(p6);   
+
+  return div;
+}
+
 
 /*
   List of LS Instructors Github username's:
